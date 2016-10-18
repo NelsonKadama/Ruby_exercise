a04cba498e547884eb2314902bf756de97b45be9
Nelson Kadama <sskadama@gmail.com>
2016-10-18 09:58:12 +0200

    added comments

--- task.rb
+++ task.rb
@@ -1,17 +1,17 @@
 
 direction_hash = Hash.new
-direction_hash["X"] = 0
-direction_hash["Y"] = 0
+direction_hash["X"] = 0   #for left/right movements
+direction_hash["Y"] = 0   #for up/down movements
 
 puts "Enter instructions in format 'DIRECTION STEPS' e.g 'DOWN 3' seperated by a comma then press enter"
 movement = gets.chomp
 movement_array = movement.split(",")
 
 movement_array.each {|element|
-  direction = element.split(" ")
+  direction = element.split(" ")  #seperates direction from steps
 
   if direction[0].upcase == "UP"
-    direction_hash["Y"] += direction[1].to_i
+    direction_hash["Y"] += direction[1].to_i    #converting string to integer
 
   elsif direction[0].upcase == "DOWN"
     direction_hash["Y"] -= direction[1].to_i
@@ -29,10 +29,12 @@ movement_array.each {|element|
 
 }
 
-direction_hash.each { |key,value|
+direction_hash.each { |key,value|   #overall X and Y coordinates
   puts "#{key} : #{value}"
 }
 puts "calculating distance"
+
+#distance = sqrt(x^2 + y^2)..
 
 sum_of_squares = direction_hash["Y"]**2 + direction_hash["X"]**2
 
